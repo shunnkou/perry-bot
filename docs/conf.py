b420ba59aaf01df4284702e4e3711c90a68a3494
Jace H <jacehuang8@protonmail.ch>
2021-02-17 23:10:04 -0500

    Test new doc

--- docs/conf.py
+++ docs/conf.py
@@ -88,8 +88,6 @@ todo_include_todos = False
 #
 html_theme = 'sphinx_book_theme'
 
-# html_theme_path = (sphinx_book_theme.get_html_theme_path()).split(" ")
-
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
 # documentation.
